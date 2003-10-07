016b10a85887162506d9625919087470879aac44
Mark Grimes <mgrimes@cpan.org>
2003-10-07 17:53:19 +0000

    Import of MGRIMES/Lirc-Client-1.16 from CPAN.
    gitpan-cpan-distribution: Lirc-Client
    gitpan-cpan-version:      1.16
    gitpan-cpan-path:         MGRIMES/Lirc-Client-1.16.tar.gz
    gitpan-cpan-author:       MGRIMES
    gitpan-cpan-maturity:     released

--- lib/Lirc/Client.pm
+++ lib/Lirc/Client.pm
@@ -3,16 +3,16 @@ package Lirc::Client;
 ###########################################################################
 # Lirc::Client
 # Mark V. Grimes
-# $Id: Client.pm,v 1.9 2003/06/27 17:16:04 mgrimes Exp $
+# $Id: Client.pm,v 1.16 2003/07/31 21:20:30 mgrimes Exp $
 #
 # Package to interact with the LIRC deamon
 # Copyright (c) 2001 Mark V. Grimes (mgrimes AT alumni DOT duke DOT edu).
 # All rights reserved. This program is free software; you can redistribute
-# it and/or modify it under the same terms as Perl itself. 
+# it and/or modify it under the same terms as Perl itself.
 #
 # Formatted with tabstops at 2
 #
-# Parts of this package were inspired by 
+# Parts of this package were inspired by
 #  hotornot.pl by michael@engsoc.org, and
 #  Perl LIRC Client (plircc) by Matti Airas (mairas@iki.fi)
 # Thanks!
@@ -20,10 +20,18 @@ package Lirc::Client;
 ###########################################################################
 
 use strict;
+use warnings;
 use IO::Socket;
+# TODO: should we use carp and croak?
+# TODO: watch for signals from lircd to re-read rc file
+
+use constant {
+		D_FAKE_LIRC => 0b010000,		# 16
+		D_PARSE			=> 0b001000,		# 8				24 = both
+	};
 
 use vars qw($VERSION);
-$VERSION = sprintf("%d.%02d", q$Revision: 1.9 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.16 $ =~ /(\d+)\.(\d+)/);
 
 # -------------------------------------------------------------------------------
 # LircClient->new( <program>, [<lircrc-file>], [<lircd-device>], [<debug-flag>] );
@@ -34,9 +42,9 @@ sub new {
   my $class     = shift;
   my $prog      = shift;
   my $lircrc    = shift || "$ENV{HOME}/.lircrc";
-  my $dname      = shift || '/dev/lircd';
-  my $debug      = shift || 0;
-  
+  my $dname     = shift || '/dev/lircd';
+  my $debug     = shift || 0;
+
   bless $self;
   $self->initialize($prog, $lircrc, $dname, $debug) or die "LircdClient couldn't initialize device $dname: $!";
   return $self;
@@ -47,15 +55,22 @@ sub new {
 sub initialize {
   my $self = shift;
   my ($prog, $lircrc, $dname, $debug) = @_;
-  
-  $self->{sock}    = IO::Socket->new(Domain => &AF_UNIX,
-                                    Type    => SOCK_STREAM,
-                                    Peer    => $dname ) or die "couldn't connect to $dname: $!";
-  $self->{prog}    = $prog;
-  $self->{rcfile} = $lircrc;
-  $self->{mode}    = '';
-  $self->{debug}  = $debug;
-  
+
+  if( $debug & D_FAKE_LIRC ){
+  	$self->{sock} = \*STDIN;
+  } else {
+		$self->{sock} = IO::Socket->new(Domain => &AF_UNIX,
+                                      Type    => SOCK_STREAM,
+                                      Peer    => $dname )
+  					or die "couldn't connect to $dname: $!";
+	}
+
+  $self->{prog}     = $prog;
+  $self->{rcfile}   = $lircrc;
+  $self->{mode}     = '';
+  $self->{debug}    = $debug;
+  $self->{in_block} = 0;
+
   $self->parse_lircrc();
 
   return 1;
@@ -65,7 +80,7 @@ sub initialize {
 
 sub clean_up {
   my $self = shift;
-  
+
   close $self->{sock};
 }
 
@@ -73,7 +88,7 @@ sub clean_up {
 
 sub parse_lircrc {
   my $self = shift;
-  
+
   local(*RCFILE);
   open( RC_FILE, "<".$self->{rcfile} ) or die "couldn't open lircrc file ($self->{rcfile}): $!";
 
@@ -85,40 +100,40 @@ sub parse_lircrc {
   while(<RC_FILE>){
     s/^\s*#.*$//g;                            # remove commented lines
 
-    print "> ($cur_mode) $_" if $self->{debug};
-    
+    print "> ($cur_mode) $_" if ($self->{debug} & D_PARSE);
+
     if     (  /^\s*begin\s*$/i            ){  # begin block
       $in_block and die "Found begin inside a block in line: $_\n";
       $in_block = 1;
-    
+
     } elsif(  /^\s*end\s*(\w*)\s*$/i          ){  # end block
       if( $1 ){
         if( $cur_mode eq $1 ){ $cur_mode = ''; next; }
         else { die "end \"$1\": found without associated begin mode"; }
       }
-      
+
       $in_block or die "Found end outside of a block in line: $_\n";
       $in_block = 0;
       defined $prog or die "end of block found without a prog code at line: $_\n";
       next if( $prog ne $self->{prog} );
       $commands{"$remote-$button-$cur_mode"} = { conf => $config, rep => $repeat, mode => $mode, flag => $flags };
-      ($prog, $remote, $button, $repeat, $config, $mode, $flags) = (undef, undef, undef, undef, undef, undef, undef, undef );  
-    
+      ($prog, $remote, $button, $repeat, $config, $mode, $flags) = (undef, undef, undef, undef, undef, undef, undef, undef );
+
     } elsif( /^\s*begin\s*(\w+)\s*$/i      ){  # begin mode block
       die "found embedded mode line: $_\n" if $cur_mode;
       die "begin mode found inside command block: $_\n" if $in_block;
       $cur_mode = $1;
-    
+
     } elsif(  /^\s*(\w+)\s*=\s*(.*?)\s*$/  ){  # command
       my ($tok, $act) = ($1, $2);
       if   ($tok =~ /^prog$/i)  { $prog    = $act; }
-      elsif($tok =~ /^remote$/i){  $remote  = $act; } 
-      elsif($tok =~ /^button$/i){  $button  = $act; } 
-      elsif($tok =~ /^repeat$/i){  $repeat  = $act; } 
-      elsif($tok =~ /^config$/i){  $config  = $act; } 
-      elsif($tok =~ /^mode$/i)  {  $mode    = $act; } 
-      elsif($tok =~ /^flags$/i)  {  $flags  = $act; } 
-    
+      elsif($tok =~ /^remote$/i){  $remote  = $act; }
+      elsif($tok =~ /^button$/i){  $button  = $act; }
+      elsif($tok =~ /^repeat$/i){  $repeat  = $act; }
+      elsif($tok =~ /^config$/i){  $config  = $act; }
+      elsif($tok =~ /^mode$/i)  {  $mode    = $act; }
+      elsif($tok =~ /^flags$/i)  {  $flags  = $act; }
+
     } elsif(  /^\s*$/                     ){  # blank line
       # do nothing
     } else {                                  # unrecognized
@@ -133,7 +148,7 @@ sub parse_lircrc {
 
 sub recognized_commands {
   my $self = shift;
-  
+
   my %commands = %{$self->{commands}};
   my @list;
   foreach my $c (keys %commands){
@@ -150,51 +165,67 @@ sub recognized_commands {
 
 # -------------------------------------------------------------------------------
 
+sub sock {
+	my $self = shift;
+	return $self->{sock};
+}
+
+# -------------------------------------------------------------------------------
+
 sub nextcode {
   my $self = shift;
-  
+
   my $fh = $self->{sock};
-  my $in_block = 0;
-  
-  while(<$fh>){
-    print "> ($in_block) $_" if $self->{debug};
-    
-    # Take care of response blocks
-    if( /^\s*BEGIN\s*$/ ){ 
-      die "got BEGIN inside a block from lircd: $_" if $in_block;
-      $in_block = 1;
-      next;
-    }
-    if( /^\s*END\s*$/ ){
-      die "got END outside a block from lircd: $_" if! $in_block;
-      $in_block = 0;
-      next;
-    }
-    next if $in_block;
+  my $command = undef;
 
-    # Decipher IR Command 
-    # http://www.lirc.org/html/technical.html#applications
-    # <hexcode> <repeat count> <button name> <remote name>
-    my ($hex, $repeat, $button, $remote) = split /\s+/;
-    defined $button and length $button or do {
-      warn "Unable to decode.\n";
-      next;
-    };
-    
-    my %commands = %{$self->{commands}};
-    my $cur_mode = $self->{mode};
-    exists $commands{"$remote-$button-$cur_mode"} or next;
-    my %command = %{$commands{"$remote-$button-$cur_mode"}};
+  while(<$fh> && (! defined $command)){
+  	$command = $self->parse_line( $_ );
+	}
+}
 
-    my $rep_count = 2**32;  # default repeat count
-    if( defined $command{rep} && $command{rep} ){ $rep_count = $command{rep}; }
-    
-    if( hex($repeat) % $rep_count != 0 ){ next; }
-    if( defined $command{mode} ){ $self->{mode} = $command{mode}; }
+# -------------------------------------------------------------------------------
 
-    print ">> $button accepted --> $command{conf}\n" if $self->{debug};
-    return $command{conf};
-  }
+sub parse_line {			# parse a line read from lircd
+	my $self = shift;
+	$_ = shift;
+
+  print "> ($self->{in_block}) $_\n" if $self->{debug};
+
+  # Take care of response blocks
+  if( /^\s*BEGIN\s*$/ ){
+		die "got BEGIN inside a block from lircd: $_" if $self->{in_block};
+		$self->{in_block} = 1;
+		return undef;
+	}
+	if( /^\s*END\s*$/ ){
+		die "got END outside a block from lircd: $_" if! $self->{in_block};
+		$self->{in_block} = 0;
+		return undef;
+	}
+	return undef if $self->{in_block};
+
+	# Decipher IR Command
+	# http://www.lirc.org/html/technical.html#applications
+	# <hexcode> <repeat count> <button name> <remote name>
+	my ($hex, $repeat, $button, $remote) = split /\s+/;
+	defined $button and length $button or do {
+		warn "Unable to decode.\n";
+		return  undef;
+	};
+
+	my %commands = %{$self->{commands}};
+	my $cur_mode = $self->{mode};
+	exists $commands{"$remote-$button-$cur_mode"} or return undef;
+	my %command = %{$commands{"$remote-$button-$cur_mode"}};
+
+	my $rep_count = 2**32;  # default repeat count
+	if( defined $command{rep} && $command{rep} ){ $rep_count = $command{rep}; }
+
+	if( hex($repeat) % $rep_count != 0 ){ return undef; }
+	if( defined $command{mode} ){ $self->{mode} = $command{mode}; }
+
+	print ">> $button accepted --> $command{conf}\n" if $self->{debug};
+	return $command{conf};
 }
 
 1;
@@ -214,15 +245,15 @@ Lirc::Client - A client library for the Linux Infrared Remote Control
   my $lirc = Lirc::Client->new( 'progname' );
   do {                            # Loop while getting ir codes
     my $code = $lirc->nextcode;    # wait for a new ir code
-    print "Lirc> $code\n";        
+    print "Lirc> $code\n";
     process( $code );              # do whatever you want with the code
   } while( defined $code );
 
 =head1 DESCRIPTION
 
-This module provides a simple interface to the Linux Infrared Remote 
+This module provides a simple interface to the Linux Infrared Remote
 Control (Lirc). The module encasuplates parsing the Lirc config file (.lircrc),
-openning a connection to the Lirc device, and blocking while waiting for 
+openning a connection to the Lirc device, and blocking while waiting for
 application specific IR commands.
 
 =head2 Use Details
@@ -232,7 +263,7 @@ application specific IR commands.
 =item new( program, [lircrc-file], [lircd-device], [debug-flag] )
 
   my $lirc = Lirc::Client->new( 'progname', "$ENV{HOME}/.lircrc", '/etc/lircd', 0 );
-  
+
 Defines the program token used in the Lirc config file, opens and parses
 the Lirc config file (defaults to ~/.lircrc if none specified), connects to
 the Lirc device (defaults to /etc/lircd if none specified), and returns the
@@ -242,21 +273,21 @@ information printed (defaults to false).
 =item nextcode()
 
   my $code = $lirc->nextcode;
-  
-Retrieves the next IR command associated with the B<progname> as defined in 
-B<new()>, blocking if none is available. 
+
+Retrieves the next IR command associated with the B<progname> as defined in
+B<new()>, blocking if none is available.
 
 =item recognized_commands()
 
   my @list = $lirc->recongnized_commands();
-  
-Returns a list of all the recongnized commands for this application (as 
+
+Returns a list of all the recongnized commands for this application (as
 defined in the call to B<new()>.
 
 =item clean_up()
 
   $lirc->clean_up();
-  
+
 Closes the Lirc device pipe, etc.
 
 =back
@@ -267,7 +298,7 @@ Closes the Lirc device pipe, etc.
 
 =item The Lirc Project - http://www.lirc.org
 
-=item Remote Roadie - http://www.peculiarities.com/RemoteRoadie/ 
+=item Remote Roadie - http://www.peculiarities.com/RemoteRoadie/
 
 My suite of perl scripts which provide both a more powerful interface layer
 between Lirc/Xmms, and various tools to manage your digital music files and
--- t/lirc-client.t
+++ t/lirc-client.t
@@ -3,138 +3,48 @@
 use strict;
 use warnings;
 
-use Test;
-
-BEGIN { plan tests => 3 }  # , todo => [4,5] }
+use Test::More tests => 3;
 
 # Test 1 -- can we load the library?
-eval { use Lirc::Client; return 1;};
-ok($@,'');
-croak() if $@;  # If module didn't load... bail hard now
+BEGIN { use_ok( 'Lirc::Client' ); };
 
-# Test 2 -- can we create an new client based on rroadie?
-my $lirc = Lirc::Client->new( 'rroadie', 'samples/lircrc' );
-ok $lirc;
+# Test 2 -- can we create an new client based on lclient_test?
+my $lirc = Lirc::Client->new( 'lclient_test', 'samples/lircrc' );
+ok $lirc, "Created new Lirc::Client";
 
 # Test 3 -- can we get the command list?
-my @commands = $lirc->recognized_commands;
-my $commands = join('',@commands);
+my %hash1 = (
+	'son-cable-CABLE_STOP-:mode' => 'undef',
+	'son-cable-CABLE_STOP-:flag' => 'undef',
+	'son-cable-CABLE_STOP-:conf' => 'STOP',
+	'son-cable-CABLE_STOP-:rep' => 'undef',
 
-my $commands_key = <<"END_COMMANDS";
-son-cable-CABLE_PLAY-:
-  conf => PLAY,
-  flag => undef,
-  rep => undef,
-  mode => undef,
+	'son-cable-CABLE_PLAY-:conf' => 'PLAY',
+	'son-cable-CABLE_PLAY-:flag' => 'undef',
+	'son-cable-CABLE_PLAY-:rep' => 'undef',
+	'son-cable-CABLE_PLAY-:mode' => 'undef',
   
-son-cable-CABLE_PAUSE-:
-  conf => PAUSE,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_ONE-:
-  conf => 1,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_REWIND-:
-  conf => PREV,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_TWO-:
-  conf => 2,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_THREE-:
-  conf => 3,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_RECALL-:
-  conf => SHUFFLE,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_STOP-:
-  conf => STOP,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_FOUR-:
-  conf => 4,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_SIX-:
-  conf => 6,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_NINE-:
-  conf => 9,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_VOL_UP-:
-  conf => VOL_UP,
-  flag => undef,
-  rep => 8,
-  mode => undef,
-  
-son-cable-CABLE_EIGHT-:
-  conf => 8,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_PIP-:
-  conf => FUNC,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_VOL_DOWN-:
-  conf => VOL_DOWN,
-  flag => undef,
-  rep => 8,
-  mode => undef,
-  
-son-cable-CABLE_FORWARD-:
-  conf => NEXT,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_SEVEN-:
-  conf => 7,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_ZERO-:
-  conf => 0,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-son-cable-CABLE_FIVE-:
-  conf => 5,
-  flag => undef,
-  rep => undef,
-  mode => undef,
-  
-END_COMMANDS
+	'son-cable-CABLE_PAUSE-:conf' => 'PAUSE',
+	'son-cable-CABLE_PAUSE-:flag' => 'undef',
+	'son-cable-CABLE_PAUSE-:rep' => 'undef',
+	'son-cable-CABLE_PAUSE-:mode' => 'undef',
+);
 
-ok( $commands, $commands_key);
+my $k;
+my %hash2;
+for ($lirc->recognized_commands){
+	next if /^\s*$/;
+	if( /:/ && ! /=>/ ){
+		s/\n.*//;
+		$k = $_;
+	} else {
+		my ($m,$v) = split /\s*=>\s*/;
+		$v =~ s/,\n.*//;
+		$hash2{"$k$m"} = $v;
+		# print "\$hash2{$k$m} = $v\n";
+	}
+}
+
+# ok eq_hash( \%hash1, \%hash2), "Recognized commands";
+is_deeply \%hash1, \%hash2, "Recognized commands";
+
